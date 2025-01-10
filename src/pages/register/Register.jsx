c1586783849c7928ce0d43a6e7cdfacf79e19827
Tanvir <zishantanvir05@gmail.com>
2025-01-10 22:23:29 +0600

    setup basic router and  lottie animation

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,60 @@
+import Lottie from "lottie-react";
+import React from "react";
+import registerLottieData from '../../assets/lottie/register.json'
+
+const Register = () => {
+    return (
+        <div>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left w-96">
+                      
+                        <Lottie animationData={registerLottieData}></Lottie>
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <h1 className="text-5xl ml-8 mt-8 font-bold">Register</h1>
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input
+                                    type="email"
+                                    placeholder="email"
+                                    className="input input-bordered"
+                                    required
+                                />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input
+                                    type="password"
+                                    placeholder="password"
+                                    className="input input-bordered"
+                                    required
+                                />
+                                <label className="label">
+                                    <a
+                                        href="#"
+                                        className="label-text-alt link link-hover"
+                                    >
+                                        Forgot password?
+                                    </a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">
+                                Register
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
